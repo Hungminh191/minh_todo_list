41b3fff67e20691d25257c954de520b9b907e605
Hungminh191 <106600810+Hungminh191@users.noreply.github.com>
2022-12-21 15:42:01 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-function myFunction() {
+function showDetail(id) {
   var x = document.getElementById("ds");
   var y = document.getElementById("detail");
   
   x.style.display = "none";
   y.style.display = "block";
     
-  var z = document.getElementById("name");
+  var z = document.getElementById(id);
   var text  = z.textContent || z.innerText;
   document.getElementById("name-detail").innerHTML = text;
   document.getElementById("name-edit").innerHTML = text;
@@ -37,10 +37,20 @@ function myFunction2() {
   document.getElementById("name-edit").innerHTML = text;
 }
 
-// function editBtn(){
-//   var x = document.getElementById("detail2");
-//   x.style.display = "block";
-// }
+function editBtn(){
+  var x = document.getElementById("detail2");
+  var y = document.getElementById("detail1");
+  x.style.display = "block";
+  y.style.display = "none";
+}
+
+function close(){
+  var x = document.getElementById("ds");
+  var y = document.getElementById("detail");
+
+  x.style.display = "block";
+  y.style.display = "none";
+}
 
 function saveEdit(){
   var x = document.getElementById("ds");
