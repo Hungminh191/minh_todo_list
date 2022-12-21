5234a050ee1d3cfc01164ba89c7528c91c6fae17
Hungminh191 <106600810+Hungminh191@users.noreply.github.com>
2022-12-21 17:26:33 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function showDetail(id) {
+  console.log('13');
   var x = document.getElementById("ds");
   var y = document.getElementById("detail");
   
@@ -11,48 +12,26 @@ function showDetail(id) {
   document.getElementById("name-edit").innerHTML = text;
 }
 
-function myFunction1() {
-  var x = document.getElementById("ds");
-  var y = document.getElementById("detail");
-
-  x.style.display = "none";
-  y.style.display = "block";
-  
-  var z = document.getElementById("name1");
-  var text  = z.textContent || z.innerText;
-  document.getElementById("name-detail").innerHTML = text;
-  document.getElementById("name-edit").innerHTML = text;
-}
-
-function myFunction2() {
-  var x = document.getElementById("ds");
-  var y = document.getElementById("detail");
-
-  x.style.display = "none";
-  y.style.display = "block";
-  
-  var z = document.getElementById("name2");
-  var text  = z.textContent || z.innerText;
-  document.getElementById("name-detail").innerHTML = text;
-  document.getElementById("name-edit").innerHTML = text;
-}
-
 function editBtn(){
+  console.log('10');
   var x = document.getElementById("detail2");
-  var y = document.getElementById("detail1");
   x.style.display = "block";
+
+  var y = document.getElementById("detail1");
   y.style.display = "none";
 }
 
-function close(){
+function close1(){
+  console.log('12');
   var x = document.getElementById("ds");
   var y = document.getElementById("detail");
-
+  
   x.style.display = "block";
   y.style.display = "none";
 }
 
 function saveEdit(){
+  console.log('11');
   var x = document.getElementById("ds");
   var y = document.getElementById("detail");
 
@@ -60,6 +39,6 @@ function saveEdit(){
   y.style.display = "none";
   
   var z = document.getElementById("name-edit").value;
-  alert(z);
-  document.getElementById("names").innerHTML = "sddsvsdvsdvsdvv";
+  // alert(z);
+  document.getElementById("name").innerHTML = z;
 }
