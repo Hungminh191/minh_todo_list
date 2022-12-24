5eaa8cf76d2d8c94f28f0778d1028fc0c3faa442
Hungminh191 <106600810+Hungminh191@users.noreply.github.com>
2022-12-24 21:46:30 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -22,142 +22,112 @@ document.getElementById("name").innerHTML = tasks[0]['name'];
 document.getElementById("time").innerHTML = tasks[0]['time'];
 document.getElementById("address").innerHTML = tasks[0]['address'];
 
-document.getElementById("name1").innerHTML = tasks[1]['name'];
-document.getElementById("time1").innerHTML = tasks[1]['time'];
-document.getElementById("address1").innerHTML = tasks[1]['address'];
+for (var i = 1; i <= tasks.length; i++) {
+  str_name = "name";
+  str_time = "time";
+  str_address = "address";
 
-document.getElementById("name2").innerHTML = tasks[2]['name'];
-document.getElementById("time2").innerHTML = tasks[2]['time'];
-document.getElementById("address2").innerHTML = tasks[2]['address'];
-
-document.getElementById("name3").innerHTML = tasks[3]['name'];
-document.getElementById("time3").innerHTML = tasks[3]['time'];
-document.getElementById("address3").innerHTML = tasks[3]['address'];
-
-document.getElementById("name4").innerHTML = tasks[4]['name'];
-document.getElementById("time4").innerHTML = tasks[4]['time'];
-document.getElementById("address4").innerHTML = tasks[4]['address'];
-
-document.getElementById("name5").innerHTML = tasks[5]['name'];
-document.getElementById("time5").innerHTML = tasks[5]['time'];
-document.getElementById("address5").innerHTML = tasks[5]['address'];
-
-document.getElementById("name6").innerHTML = tasks[6]['name'];
-document.getElementById("time6").innerHTML = tasks[6]['time'];
-document.getElementById("address6").innerHTML = tasks[6]['address'];
-
-document.getElementById("name7").innerHTML = tasks[7]['name'];
-document.getElementById("time7").innerHTML = tasks[7]['time'];
-document.getElementById("address7").innerHTML = tasks[7]['address'];
-
-document.getElementById("name8").innerHTML = tasks[8]['name'];
-document.getElementById("time8").innerHTML = tasks[8]['time'];
-document.getElementById("address8").innerHTML = tasks[8]['address'];
-
-document.getElementById("name9").innerHTML = tasks[9]['name'];
-document.getElementById("time9").innerHTML = tasks[9]['time'];
-document.getElementById("address9").innerHTML = tasks[9]['address'];
-
-document.getElementById("name10").innerHTML = tasks[10]['name'];
-document.getElementById("time10").innerHTML = tasks[10]['time'];
-document.getElementById("address10").innerHTML = tasks[10]['address'];
-
-document.getElementById("name11").innerHTML = tasks[11]['name'];
-document.getElementById("time11").innerHTML = tasks[11]['time'];
-document.getElementById("address11").innerHTML = tasks[11]['address'];
+  str_name = str_name + i; 
+  str_time = str_time + i; 
+  str_address = str_address + i;
+  
+  document.getElementById(str_name).innerHTML = tasks[i]['name'];
+  document.getElementById(str_time).innerHTML = tasks[i]['time'];
+  document.getElementById(str_address).innerHTML = tasks[i]['address'];
+  
+}
 
 function showDetail(name, time, address, task) {
-  console.log('12');
   idname = name;
   idtime = time;
   idaddress = address;
   idtask = task;
 
-  var x = document.getElementById("ds");
-  var y = document.getElementById("detail");
+  var id_ds = document.getElementById("ds");
+  var id_detail = document.getElementById("detail");
 
-  var a = document.getElementById("detail1");
-  var b = document.getElementById("detail2");
-  var c = document.getElementById("detail3");
+  var id_detail1 = document.getElementById("detail1");
+  var id_detail2 = document.getElementById("detail2");
+  var id_detail3 = document.getElementById("detail3");
 
-  var disx = window.getComputedStyle(x).display;
-  var disa = window.getComputedStyle(a).display;
+  var disx = window.getComputedStyle(id_ds).display;
+  var disa = window.getComputedStyle(id_detail1).display;
 
   if (disx === "block") {
-    x.style.display = "none";
-    y.style.display = "block";
-    a.style.display = "block";
-    b.style.display = "none";
-    c.style.display = "none";
+    id_ds.style.display = "none";
+    id_detail.style.display = "block";
+
+    id_detail1.style.display = "block";
+    id_detail2.style.display = "none";
+    id_detail3.style.display = "none";
   } else if (disa === "none") {
-    a.style.display = "block";
-    b.style.display = "none";
-    c.style.display = "none";
+    id_detail1.style.display = "block";
+    id_detail2.style.display = "none";
+    id_detail3.style.display = "none";
   }
 
-  var z = document.getElementById(name);
-  var text  = z.textContent || z.innerText;
+  var id_name = document.getElementById(name);
+  var text  = id_name.textContent || id_name.innerText;
 
   document.getElementById("name-detail").innerHTML = text;
   document.getElementById("name-edit").innerHTML = text;
 
-  var t = document.getElementById(time);
-  var textTime = t.textContent || t.innerText;
+  var id_time = document.getElementById(time);
+  var textTime = id_time.textContent || id_time.innerText;
 
   document.getElementById("time-detail").innerHTML = textTime;
   document.getElementById("time-edit").value = textTime;
 
 
-  var v = document.getElementById(address);
-  var textAddress = v.textContent || v.innerText;
+  var id_address = document.getElementById(address);
+  var textAddress = id_address.textContent || id_address.innerText;
 
   document.getElementById("address-detail").innerHTML = textAddress;
   document.getElementById("address-edit").value = textAddress;
 }
 
 function editBtn(){
-  var x = document.getElementById("detail2");
-  x.style.display = "block";
+  var id_detail2 = document.getElementById("detail2");
+  id_detail2.style.display = "block";
 
-  var y = document.getElementById("detail1");
-  y.style.display = "none";
+  var id_detail1 = document.getElementById("detail1");
+  id_detail1.style.display = "none";
 }
 
 function close1(){
-  var x = document.getElementById("ds");
-  var y = document.getElementById("detail");
+  var id_ds = document.getElementById("ds");
+  var id_detail = document.getElementById("detail");
   
-  x.style.display = "block";
-  y.style.display = "none";
+  id_ds.style.display = "block";
+  id_detail.style.display = "none";
 }
 
 function saveEdit(){
-  console.log('11');
-  var x = document.getElementById("ds");
-  var y = document.getElementById("detail");
+  var id_ds = document.getElementById("ds");
+  var id_detail = document.getElementById("detail");
 
-  x.style.display = "block";
-  y.style.display = "none";
+  id_ds.style.display = "block";
+  id_detail.style.display = "none";
   
-  var z = document.getElementById("name-edit").value;
-  var t = document.getElementById("time-edit").value;
-  var v = document.getElementById("address-edit").value;
+  var id_name_edit = document.getElementById("name-edit").value;
+  var id_time_edit = document.getElementById("time-edit").value;
+  var id_address_edit = document.getElementById("address-edit").value;
   // console.log(t);
-  document.getElementById(idname).innerHTML = z;
-  document.getElementById(idtime).innerHTML = t;
-  document.getElementById(idaddress).innerHTML = v;
+  document.getElementById(idname).innerHTML = id_name_edit;
+  document.getElementById(idtime).innerHTML = id_time_edit;
+  document.getElementById(idaddress).innerHTML = id_address_edit;
 }
 
 function createBtn(){
-  var x = document.getElementById("ds");
-  var y = document.getElementById("detail");
-  var z = document.getElementById("detail3");
-  var t = document.getElementById("detail1");
+  var id_ds = document.getElementById("ds");
+  var id_detail = document.getElementById("detail");
+  var id_detail3 = document.getElementById("detail3");
+  var id_detail1 = document.getElementById("detail1");
 
-  x.style.display = "none";
-  y.style.display = "block";
-  z.style.display = "block";
-  t.style.display = "none";
+  id_ds.style.display = "none";
+  id_detail.style.display = "block";
+  id_detail3.style.display = "block";
+  id_detail1.style.display = "none";
 }
 
 function addBtn(){
@@ -166,7 +136,6 @@ function addBtn(){
   checkbox.type = "checkbox";
   
   var para = document.createElement('p');
-  // var node = document.createTextNode("Họp dự án");
   var node = document.getElementById("name-add").value;
   var title = document.createTextNode(node);
   console.log(node);
@@ -208,7 +177,91 @@ function addBtn(){
   
   var element = document.getElementById("list");
   var child = document.getElementById("task");
-  // element.appendChild(span);
+  element.insertBefore(span, child);
+
+  var x = document.getElementById("ds");
+  var y = document.getElementById("detail");
+
+  x.style.display = "block";
+  y.style.display = "none";
+}
+
+function addBtn1(){
+  
+  var checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  
+  var span2 = document.createElement('span');
+  span2.id = "name0";
+
+  var node2 = document.getElementById("name-add").value;
+  // console.log(node2); return;
+  var title2 = document.createTextNode(node2);
+  // console.log(title2); return;
+  span2.appendChild(title2);
+
+  var para1 = document.createElement('p');
+  para1.id = "time0";
+  
+  var node1 = document.getElementById("time-add").value;
+  // console.log(node1); return;
+  var title1 = document.createTextNode(node1);
+  para1.appendChild(title1);
+
+  var para3 = document.createElement('p');
+  para3.id = "address0";
+
+  var node3 = document.getElementById("address-add").value;
+  var title3 = document.createTextNode(node3);
+  para3.appendChild(title3);
+
+  // // var para = document.createElement('p');
+  // // var node = document.getElementById("name-add").value;
+  // var title = document.createTextNode(node);
+  // console.log(node);
+  // para.appendChild(title);
+  
+  var span1 = document.createElement('span');
+  span1.appendChild(checkbox);
+  span1.appendChild(span2);
+  span1.appendChild(para1);
+  span1.appendChild(para3);
+  span1.setAttribute("onclick", "showDetail(name0, time0, address0, task0)");
+  span1.className = "option";
+
+  var a = document.createElement('a');
+  var linkText = document.createTextNode("Đóng");
+  a.appendChild(linkText);
+  a.href = "#";
+  
+  var a1 = document.createElement('a');
+  var linkText1 = document.createTextNode("Xoá");
+  a1.appendChild(linkText1);
+  a1.setAttribute("onclick", "removeTask(task0)");
+
+  var div1 = document.createElement('div');
+  div1.appendChild(a);
+  div1.appendChild(a1);
+  div1.className = "dropup-content";
+
+  var btn = document.createElement('button');
+  btn.innerHTML = "<strong>&vellip;</strong>";
+  btn.className = "dropbtn";
+  
+  var div = document.createElement('div');
+  div.appendChild(btn);
+  div.appendChild(div1);
+  div.className = "dropup";
+
+  var span = document.createElement('span');
+  span.appendChild(span1);
+  span.appendChild(div);
+  span.className = "option1";
+  span.id = "task0";
+  
+  
+  var element = document.getElementById("list");
+  var child = document.getElementById("task");
   element.insertBefore(span, child);
 
   var x = document.getElementById("ds");
